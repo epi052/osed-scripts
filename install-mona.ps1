23b95476cabe3118ab1d4a2e67c56180d3c3d4d3
epi <43392618+epi052@users.noreply.github.com>
2021-03-13 10:40:06 -0600

    Update install-mona.ps1

--- install-mona.ps1
+++ install-mona.ps1
@@ -1,15 +1,15 @@
 $share_path = "\\tsclient\mona-share\"
-$insall_dir = "C:\Users\Offsec\Desktop\install-monas"
+$install_dir = "C:\Users\Offsec\Desktop\install-monas"
 
-mkdir $insall_dir
+mkdir $install_dir
 
 # move Offsec's pykd files out of the way
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd.bak"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll.bak"
 
 # install python2.7
-copy "$share_path\python-2.7.17.msi" $insall_dir
-msiexec.exe /i $insall_dir\python-2.7.17.msi /qn
+copy "$share_path\python-2.7.17.msi" $install_dir
+msiexec.exe /i $install_dir\python-2.7.17.msi /qn
 start-sleep 10
 
 # register Python2.7 binaries in path before Python3
@@ -18,8 +18,8 @@ $p = [System.Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVari
 
 
 # install old c++ runtime
-copy "$share_path\vcredist_x86.exe" $insall_dir
-$insall_dir\vcredist_x86.exe /Q
+copy "$share_path\vcredist_x86.exe" $install_dir
+$install_dir\vcredist_x86.exe /Q
 start-sleep 10
 
 # register runtime debug dll
