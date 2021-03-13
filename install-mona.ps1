9216abcfda2ebb29438a32798bd248c3dafec29b
epi <43392618+epi052@users.noreply.github.com>
2021-03-13 10:46:38 -0600

    Update install-mona.ps1

--- install-mona.ps1
+++ install-mona.ps1
@@ -1,32 +1,39 @@
 $share_path = "\\tsclient\mona-share\"
 $install_dir = "C:\Users\Offsec\Desktop\install-monas"
 
+echo "[+] creating installation directory: $install_dir"
 mkdir $install_dir
 
-# move Offsec's pykd files out of the way
+echo "[+] backing up old pykd files"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd.bak"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll.bak"
 
 # install python2.7
+
+echo "[+] installing python2.7"
 copy "$share_path\python-2.7.17.msi" $install_dir
 msiexec.exe /i $install_dir\python-2.7.17.msi /qn
 start-sleep 10
 
 # register Python2.7 binaries in path before Python3
+echo "[+] adding python2.7 to the PATH"
 $p = [System.Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('Path',"C:\Python27\;C:\Python27\Scripts;"+$p,[System.EnvironmentVariableTarget]::User)
 
 
 # install old c++ runtime
+echo "[+] installing old c++ runtime"
 copy "$share_path\vcredist_x86.exe" $install_dir
 $install_dir\vcredist_x86.exe /Q
 start-sleep 10
 
 # register runtime debug dll
+echo "[+] registering runtime debug dll"
 cd "C:\Program Files\Common Files\Microsoft Shared\VC"
 regsvr32 /s msdia90.dll
 
 # copy mona files
+echo "[+] bringing over mona files and fresh pykd"
 copy "$share_path\windbglib.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
 copy "$share_path\mona.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
 copy "$share_path\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext"
