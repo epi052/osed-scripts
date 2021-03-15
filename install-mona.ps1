3ff51c7d3d628fac93e5fb21dc0ffd16c1545972
Ben "epi" Risher <epibar052@gmail.com>
2021-03-14 19:30:54 -0500

    nitpickery

--- install-mona.ps1
+++ install-mona.ps1
@@ -4,6 +4,13 @@ $install_dir = "C:\Users\Offsec\Desktop\install-mona"
 echo "[+] creating installation directory: $install_dir"
 mkdir $install_dir
 
+# install old c++ runtime
+echo "[+] installing old c++ runtime"
+copy "$share_path\vcredist_x86.exe" $install_dir
+cd $install_dir
+.\vcredist_x86.exe 
+start-sleep 10
+
 echo "[+] backing up old pykd files"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.pyd.bak"
 move "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext\pykd.dll.bak"
@@ -20,21 +27,16 @@ echo "[+] adding python2.7 to the PATH"
 $p = [System.Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('Path',"C:\Python27\;C:\Python27\Scripts;"+$p,[System.EnvironmentVariableTarget]::User)
 
-
-# install old c++ runtime
-echo "[+] installing old c++ runtime"
-copy "$share_path\vcredist_x86.exe" $install_dir
-cd $install_dir
-.\vcredist_x86.exe 
-read-host "press enter once you've installed the runtime"
+# copy mona files
+echo "[+] bringing over mona files and fresh pykd"
+copy "$share_path\windbglib.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
+copy "$share_path\mona.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
+copy "$share_path\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext"
 
 # register runtime debug dll
 echo "[+] registering runtime debug dll"
 cd "C:\Program Files\Common Files\Microsoft Shared\VC"
 regsvr32 /s msdia90.dll
 
-# copy mona files
-echo "[+] bringing over mona files and fresh pykd"
-copy "$share_path\windbglib.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
-copy "$share_path\mona.py" "C:\Program Files\Windows Kits\10\Debuggers\x86"
-copy "$share_path\pykd.pyd" "C:\Program Files\Windows Kits\10\Debuggers\x86\winext"
+echo "[=] in case you see something about symbols when running mona, try executing the following (the runtime took too long to install)"
+echo "regsvr32 "C:\Program Files\Common Files\Microsoft Shared\VC\msdia90.dll"
